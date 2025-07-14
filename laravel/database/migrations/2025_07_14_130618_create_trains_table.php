b441c110384ac2ab57bb4ecee06063d997ca06b9
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-07-14 16:17:45 +0200

    ADD migrations

---
+++ laravel/database/migrations/2025_07_14_130618_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->char('codice_treno', 10)->unique();
+            $table->integer('totale_carrozze')->nullable();
+            $table->boolean('arriva_in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
