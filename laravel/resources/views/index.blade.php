8b2541153f5e6e607f13542dfafdff760fce53dc
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-07-14 17:21:48 +0200

    END without css

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -11,10 +11,32 @@
     @foreach($trains as $train)
 
         <p>
-            <h3>treno numero:</h3>
-            <span> {{ $train->codice_treno }}</span>
-            
-        </p>
+            <h3> treno numero: {{ $train->codice_treno }}</h3>
+            <p> operato da : {{ $train->azienda }}</p>
+            <p> in arrivo da : {{ $train->stazione_di_partenza }}</p>
+            <p> diretto a : {{ $train->stazione_di_arrivo }}</p>
+            <p> partirà alle : {{ $train->orario_di_partenza }}</p>
+            <p> al binario : {{ $train->binario }}</p>
+            <p> arriverà alle : {{ $train->orario_di_arrivo }}</p>
+            <p> in arrivo da : {{ $train->stazione_di_partenza }}</p>
+            <p> è previsto ritardo : 
+                
+                @if($train->arriva_in_orario)
+                    No
+                @else
+                    Sì
+                @endif
+                
+            </p>
+            <p> è prevista cancellazione : 
+                
+                @if($train->cancellato)
+                    Si
+                @else
+                    No
+                @endif
+                
+            </p>        
 
     @endforeach
 </body>
